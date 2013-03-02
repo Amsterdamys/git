970102f9b51402019635c3d98dbe5afd4ad800c3
Amsterdamys <zarkovsky@yandex.ru>
2013-03-02 18:12:52 +0400

    Js editor
    Upload js editor

---
+++ js_editor/js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+    var canvas = new fabric.Canvas('c');
+    canvas.isDrawingMode = true;
+    canvas.freeDrawingColor = '#000';
+    canvas.freeDrawingLineWidth = 3;
+    canvas.hasBorders = false;
+    canvas.hasControls = false;
+})
