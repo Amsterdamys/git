0273e20a008a15e15e695838f9ecf9bb3321816d
Amsterdamys <zarkovsky@yandex.ru>
2013-03-02 18:22:08 +0400

    Revert "Js editor"
    This reverts commit 970102f9b51402019635c3d98dbe5afd4ad800c3.

--- js_editor/js/script.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    var canvas = new fabric.Canvas('c');
-    canvas.isDrawingMode = true;
-    canvas.freeDrawingColor = '#000';
-    canvas.freeDrawingLineWidth = 3;
-    canvas.hasBorders = false;
-    canvas.hasControls = false;
-})
